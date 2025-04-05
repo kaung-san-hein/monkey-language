51d2703bff87c6af8cd97294d52f09f9f77a5dff
kaung-san-hein <kaungsanhein2019@gmail.com>
2025-04-05 22:08:51 +0900

    add buildin puts fun

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -1,6 +1,10 @@
 package evaluator
 
-import "github.com/kaung-san-hein/monkey/object"
+import (
+	"fmt"
+
+	"github.com/kaung-san-hein/monkey/object"
+)
 
 var builtins = map[string]*object.Builtin{
 	"len": {
@@ -106,6 +110,14 @@ var builtins = map[string]*object.Builtin{
 			newElements[length] = args[1]
 
 			return &object.Array{Elements: newElements}
+		},
+	},
+	"puts": {
+		Fn: func(args ...object.Object) object.Object {
+			for _, arg := range args {
+				fmt.Println(arg.Inspect())
+			}
+			return NULL
 		},
 	},
 }
