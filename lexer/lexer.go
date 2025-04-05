9ecfed61a043983bd05ab017566f6422122daa90
kaung-san-hein <kaungsanhein2019@gmail.com>
2025-04-05 09:20:48 +0900

    add string data type

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -71,6 +71,9 @@ func (l *Lexer) NextToken() token.Token {
 		tok = newToken(token.LBRACE, l.ch)
 	case '}':
 		tok = newToken(token.RBRACE, l.ch)
+	case '"':
+		tok.Type = token.STRING
+		tok.Literal = l.readString()
 	case 0:
 		tok = newToken(token.RBRACE, l.ch)
 		tok.Literal = ""
@@ -132,4 +135,17 @@ func (l *Lexer) peekChar() byte {
 	} else {
 		return l.input[l.readPosition]
 	}
+}
+
+func (l *Lexer) readString() string {
+	position := l.position + 1
+	for {
+		l.readChar()
+
+		if l.ch == '"' || l.ch == 0 {
+			break
+		}
+	}
+
+	return l.input[position:l.position]
 }
